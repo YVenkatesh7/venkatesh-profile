3facf11eff5a1d4b445e8b0e4b3a0bed5b798c15
saisaira <saitarakdevarapalli@gmail.com>
2020-04-29 10:03:36 +0530

    Updates

---
+++ precache-manifest.ecd6debc40c311de4f09225f702f9e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa522ed2c22b13ab8dfa24ce17d0992",
+    "url": "/venkatesh-profile/index.html"
+  },
+  {
+    "revision": "53ae4f9d81f711fe9e94",
+    "url": "/venkatesh-profile/static/css/main.575f0ec9.chunk.css"
+  },
+  {
+    "revision": "74db671685aecc3bfd7a",
+    "url": "/venkatesh-profile/static/js/2.c1de257a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/venkatesh-profile/static/js/2.c1de257a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ae4f9d81f711fe9e94",
+    "url": "/venkatesh-profile/static/js/main.80a01063.chunk.js"
+  },
+  {
+    "revision": "65b18918cb9edb6854ae",
+    "url": "/venkatesh-profile/static/js/runtime-main.1250bae9.js"
+  },
+  {
+    "revision": "cb633f7aa733dd10f355ac4690cc8129",
+    "url": "/venkatesh-profile/static/media/mypic.cb633f7a.jpg"
+  }
+]);
